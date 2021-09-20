e48e9af20b853c30f6a3e3164eac7c654de168bc
annguyen <annguyen.it.utc@gmail.com>
2021-09-20 15:54:45 +0700

    Ex6

---
+++ 17_9_21/Ex6/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Ex6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstList = new System.Windows.Forms.ListBox();
+            this.btnComfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstList
+            // 
+            this.lstList.FormattingEnabled = true;
+            this.lstList.ItemHeight = 16;
+            this.lstList.Location = new System.Drawing.Point(63, 39);
+            this.lstList.MultiColumn = true;
+            this.lstList.Name = "lstList";
+            this.lstList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstList.Size = new System.Drawing.Size(408, 260);
+            this.lstList.TabIndex = 0;
+            // 
+            // btnComfirm
+            // 
+            this.btnComfirm.Location = new System.Drawing.Point(227, 329);
+            this.btnComfirm.Name = "btnComfirm";
+            this.btnComfirm.Size = new System.Drawing.Size(75, 23);
+            this.btnComfirm.TabIndex = 1;
+            this.btnComfirm.Text = "OK";
+            this.btnComfirm.UseVisualStyleBackColor = true;
+            this.btnComfirm.Click += new System.EventHandler(this.btnComfirm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 384);
+            this.Controls.Add(this.btnComfirm);
+            this.Controls.Add(this.lstList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstList;
+        private System.Windows.Forms.Button btnComfirm;
+    }
+}
+
---
+++ 17_9_21/Ex6/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex6
+{
+    public partial class Form1 : Form
+    {
+        private const string messageBoxTitle = "Thông báo";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstList.ColumnWidth = lstList.Width / 4;
+            for (int i = 1; i <= 100; i++)
+            {
+                lstList.Items.Add("Item " + i.ToString());
+            }
+        }
+
+        private void btnComfirm_Click(object sender, EventArgs e)
+        {
+            string selectedItems = String.Join(", ", lstList.SelectedItems.Cast<string>());
+            string message = "Bạn đã chọn các phần tử: " + selectedItems;
+            MessageBox.Show(message, messageBoxTitle);
+        }
+    }
+}
