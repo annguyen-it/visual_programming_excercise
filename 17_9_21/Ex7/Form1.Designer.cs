4cb860b623ba951b5df31ba197aaac4b49b6fad4
annguyen <annguyen.it.utc@gmail.com>
2021-09-20 15:59:20 +0700

    Ex7

---
+++ 17_9_21/Ex7/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Ex7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboDirectory = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cboDirectory
+            // 
+            this.cboDirectory.FormattingEnabled = true;
+            this.cboDirectory.Location = new System.Drawing.Point(128, 47);
+            this.cboDirectory.Name = "cboDirectory";
+            this.cboDirectory.Size = new System.Drawing.Size(420, 24);
+            this.cboDirectory.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 205);
+            this.Controls.Add(this.cboDirectory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboDirectory;
+    }
+}
+
