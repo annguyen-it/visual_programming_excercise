910883969b088ab296d92db3ff0191de00a6d7a1
annguyen <annguyen.it.utc@gmail.com>
2021-09-22 23:45:03 +0700

    Ex10

---
+++ 17_9_21/Ex10/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Ex10
+{
+    partial class QuizForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grbCountry = new System.Windows.Forms.GroupBox();
+            this.grbCapital = new System.Windows.Forms.GroupBox();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // grbCountry
+            // 
+            this.grbCountry.Location = new System.Drawing.Point(31, 26);
+            this.grbCountry.Name = "grbCountry";
+            this.grbCountry.Size = new System.Drawing.Size(200, 100);
+            this.grbCountry.TabIndex = 0;
+            this.grbCountry.TabStop = false;
+            this.grbCountry.Text = "Country";
+            // 
+            // grbCapital
+            // 
+            this.grbCapital.Location = new System.Drawing.Point(299, 26);
+            this.grbCapital.Name = "grbCapital";
+            this.grbCapital.Size = new System.Drawing.Size(200, 100);
+            this.grbCapital.TabIndex = 0;
+            this.grbCapital.TabStop = false;
+            this.grbCapital.Text = "Capital";
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Location = new System.Drawing.Point(28, 146);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(0, 17);
+            this.lblHint.ForeColor = System.Drawing.Color.Red;
+            this.lblHint.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(424, 146);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 196);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.grbCapital);
+            this.Controls.Add(this.grbCountry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grbCountry;
+        private System.Windows.Forms.GroupBox grbCapital;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 17_9_21/Ex10/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex10
+{
+    public partial class QuizForm : Form
+    {
+        private readonly Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        RadioButton checkedCountryRadio;
+
+        public QuizForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Tuple<List<string>, List<string>> data = LoadData();
+
+            LoadCountryRadios(data.Item1);
+            LoadCapitalRadios(data.Item2);
+
+            MoveExitButton();
+            MoveHintLabel();
+
+            Size = new Size(Size.Width, btnExit.Location.Y + btnExit.Size.Height + 50);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private Tuple<List<string>, List<string>> LoadData()
+        {
+            dictionary.Add("France", "Paris");
+            dictionary.Add("Japan", "Tokyo");
+            dictionary.Add("Hungary", "Budapest");
+            dictionary.Add("Spain", "Madrid");
+            dictionary.Add("Turkey", "Ankara");
+            dictionary.Add("The UK", "London");
+            dictionary.Add("Italy", "Rome");
+            dictionary.Add("Argentina", "Buenos Aires");
+            dictionary.Add("The US", "Washington");
+
+            List<string> countries = new List<string>();
+            List<string> capitals = new List<string>();
+
+            foreach (var item in dictionary)
+            {
+                countries.Add(item.Key);
+                capitals.Add(item.Value);
+            }
+
+            countries.Sort();
+            capitals.Sort();
+
+            return Tuple.Create(countries, capitals);
+        }
+
+        private void LoadCountryRadios(List<string> countries)
+        {
+            int index = 0;
+            foreach (string country in countries)
+            {
+                int y = grbCountry.Location.Y + 25 * index++ - 5;
+
+                RadioButton rb = new RadioButton
+                {
+                    Text = country,
+                    Location = new Point(20, y)
+                };
+
+                rb.Click += new EventHandler((object sender, EventArgs e) =>
+                {
+                    lblHint.Text = "Bạn hãy chọn thủ đô cho " + country;
+                    grbCapital.Enabled = true;
+                    checkedCountryRadio = rb;
+                });
+
+                grbCountry.Controls.Add(rb);
+            }
+
+            grbCountry.Size = new Size(grbCountry.Size.Width, grbCountry.Location.Y + 20 * index + 45);
+        }
+
+        private void LoadCapitalRadios(List<string> capitals)
+        {
+            int index = 0;
+            foreach (string capital in capitals)
+            {
+                int y = grbCapital.Location.Y + 25 * index++ - 5;
+
+                RadioButton rb = new RadioButton
+                {
+                    Text = capital,
+                    Location = new Point(20, y)
+                };
+
+                rb.Click += new EventHandler((object sender, EventArgs e) =>
+                {
+                    string checkedCountry = checkedCountryRadio.Text;
+                    if (dictionary[checkedCountry].Equals(capital))
+                        MessageBox.Show(String.Format("Chúc mừng bạn, thủ đô của {0} là {1}", checkedCountry, capital));
+
+                    else
+                        MessageBox.Show(String.Format("Bạn sai rồi, thủ đô của {0} không phải là {1}", checkedCountry, capital));
+                });
+
+                grbCapital.Controls.Add(rb);
+            }
+
+            grbCapital.Size = new Size(grbCapital.Size.Width, grbCapital.Location.Y + 20 * index + 45);
+            grbCapital.Enabled = false;
+        }
+
+        private void MoveExitButton()
+        {
+            btnExit.Location = new Point(
+                btnExit.Location.X,
+                Math.Max(grbCountry.Size.Height, grbCapital.Size.Height)
+                    + grbCountry.Location.Y
+                    + 20);
+        }
+
+        private void MoveHintLabel()
+        {
+            lblHint.Location = new Point(
+                lblHint.Location.X,
+                btnExit.Location.Y);
+        }
+    }
+}
