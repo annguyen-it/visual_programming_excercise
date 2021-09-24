31b520ff4325d4bf6e4bf176ef3c7f67ace110a1
annguyen <annguyen.it.utc@gmail.com>
2021-09-24 16:03:24 +0700

    Ex15

---
+++ 24_9_21/Ex15/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Ex15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lblSecond = new System.Windows.Forms.Label();
+            this.lblSeperator = new System.Windows.Forms.Label();
+            this.txtMinute = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Location = new System.Drawing.Point(128, 30);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(37, 17);
+            this.lblMinute.TabIndex = 0;
+            this.lblMinute.Text = "Phút";
+            // 
+            // lblSecond
+            // 
+            this.lblSecond.AutoSize = true;
+            this.lblSecond.Location = new System.Drawing.Point(285, 30);
+            this.lblSecond.Name = "lblSecond";
+            this.lblSecond.Size = new System.Drawing.Size(37, 17);
+            this.lblSecond.TabIndex = 1;
+            this.lblSecond.Text = "Giây";
+            // 
+            // lblSeperator
+            // 
+            this.lblSeperator.AutoSize = true;
+            this.lblSeperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeperator.Location = new System.Drawing.Point(219, 59);
+            this.lblSeperator.Name = "lblSeperator";
+            this.lblSeperator.Size = new System.Drawing.Size(15, 20);
+            this.lblSeperator.TabIndex = 2;
+            this.lblSeperator.Text = ":";
+            // 
+            // txtMinute
+            // 
+            this.txtMinute.Location = new System.Drawing.Point(97, 59);
+            this.txtMinute.Name = "txtMinute";
+            this.txtMinute.Size = new System.Drawing.Size(100, 22);
+            this.txtMinute.TabIndex = 3;
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(255, 59);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(100, 22);
+            this.txtSecond.TabIndex = 4;
+            this.txtSecond.TextChanged += new System.EventHandler(this.txtSecond_TextChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Enabled = false;
+            this.btnRun.Location = new System.Drawing.Point(76, 117);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(89, 32);
+            this.btnRun.TabIndex = 5;
+            this.btnRun.Text = "Chạy";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(185, 117);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(89, 32);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "Tạm dừng";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(299, 117);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(89, 32);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Kết thúc";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 1000;
+            this.tmr.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 172);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtMinute);
+            this.Controls.Add(this.lblSeperator);
+            this.Controls.Add(this.lblSecond);
+            this.Controls.Add(this.lblMinute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Label lblSecond;
+        private System.Windows.Forms.Label lblSeperator;
+        private System.Windows.Forms.TextBox txtMinute;
+        private System.Windows.Forms.TextBox txtSecond;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer tmr;
+    }
+}
+
---
+++ 24_9_21/Ex15/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ex15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSecond_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtSecond.Text, out _))
+            {
+                btnRun.Enabled = true;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtMinute.Text, out _))
+                txtMinute.Text = "0";
+
+            tmr.Start();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            tmr.Stop();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            txtMinute.Text = "0";
+            txtSecond.Text = "0";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int m = int.Parse(txtMinute.Text);
+            int s = int.Parse(txtSecond.Text);
+
+            if (s > 0)
+            {
+                txtSecond.Text = (s - 1).ToString();
+            }
+            else if (m > 0)
+            {
+                txtSecond.Text = "59";
+                txtMinute.Text = (m - 1).ToString();
+            }
+            else
+            {
+                txtMinute.Text = "0";
+                tmr.Stop();
+            }
+        }
+    }
+}
