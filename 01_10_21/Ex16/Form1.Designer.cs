413ccf9a9256058164792ee43ee43ce025ea9474
annguyen <annguyen.it.utc@gmail.com>
2021-10-01 10:13:01 +0700

    Ex16

---
+++ 01_10_21/Ex16/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace Ex16
+{
+    partial class frmPicture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPicture = new System.Windows.Forms.PictureBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPicture
+            // 
+            this.picPicture.Location = new System.Drawing.Point(182, 37);
+            this.picPicture.Name = "picPicture";
+            this.picPicture.Size = new System.Drawing.Size(298, 208);
+            this.picPicture.TabIndex = 0;
+            this.picPicture.TabStop = false;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(297, 276);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 32);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // frmPicture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 330);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.picPicture);
+            this.Name = "frmPicture";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPicture;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ 01_10_21/Ex16/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex16
+{
+    public partial class frmPicture : Form
+    {
+        public frmPicture()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgOpen = new OpenFileDialog
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*",
+                InitialDirectory = "C:\\",
+                Title = "Chọn ảnh để hiển thị"
+            };
+
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+                picPicture.Image = Image.FromFile(dlgOpen.FileName);
+            else
+                MessageBox.Show(
+                    "You clicked Cancel",
+                    "Open Dialog",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+        }
+    }
+}
